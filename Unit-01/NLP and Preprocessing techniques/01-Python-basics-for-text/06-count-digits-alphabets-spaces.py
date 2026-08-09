278cd94b44af4a00c64ee32d3513a4e99c0f9478
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-09 12:22:44 +0530

    feat(nlp): add character type classifier and counter script

---
+++ Unit-01/NLP and Preprocessing techniques/01-Python-basics-for-text/06-count-digits-alphabets-spaces.py
@@ -0,0 +1,16 @@
+text = "Python NLP 2026"
+letters = 0
+digits = 0
+spaces = 0
+
+for ch in text:
+    if ch.isalpha():
+        letters += 1
+    elif ch.isdigit():
+        digits += 1
+    elif ch.isspace():
+        spaces += 1
+
+print("Letters: ",letters)
+print("Digits: ",digits)
+print("Spaces: ",spaces)
